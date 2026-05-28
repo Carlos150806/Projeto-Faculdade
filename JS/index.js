7e57a3ed437ebee6b31413303049f2381dbdea0e
Carlos150806 <Carlos.eduardo15080622@gmail.com>
2026-05-28 19:54:30 -0300

    Atualização index (polimento impacto / polimento sobre)

--- JS/index.js
+++ JS/index.js
@@ -11,20 +11,24 @@ imagem2.addEventListener ("mouseenter", texto2)
 imagem2.addEventListener ("mouseout", sair2)
 
 function texto1(){
-    txt[0].style.display = "block"
+    txt[0].style.opacity = '1';
+    txt[0].style.visibility = 'visible';
 }
 
 function texto2(){
     const txt = document.getElementsByClassName("txt_sobreposto")
-    txt[1].style.display = "block"
+    txt[1].style.opacity = '1';
+    txt[1].style.visibility = 'visible';
 }
 
 function sair1(){
     const txt = document.getElementsByClassName("txt_sobreposto")
-    txt[0].style.display = "none"
+    txt[0].style.opacity = '0';
+    txt[0].style.visibility = 'hidden';
 }
 
 function sair2(){
     const txt = document.getElementsByClassName("txt_sobreposto")
-    txt[1].style.display = "none"
+    txt[1].style.opacity = '0';
+    txt[1].style.visibility = 'hidden';
 }
