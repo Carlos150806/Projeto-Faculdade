93d50608d26f8b529940d86c74608283d70eade9
Carlos150806 <carlos.eduardo15080622@gmail.com>
2026-05-29 00:04:51 -0300

    Atualização index (polimento sobre e inicialização serviços)

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+//EFEITOS NA PARTE DO SOBRE
+
 const txt = document.getElementsByClassName("txt_sobreposto")
 txt[0].addEventListener ("mouseenter",texto1)
 txt[1].addEventListener ("mouseenter",texto2)
@@ -31,4 +33,59 @@ function sair2(){
     const txt = document.getElementsByClassName("txt_sobreposto")
     txt[1].style.opacity = '0';
     txt[1].style.visibility = 'hidden';
+}
+
+//EFEITOS NA PARTE DOS SERVIÇOS
+
+const texto = document.getElementsByClassName("txtFlu")
+
+texto[0].addEventListener ("mouseenter", entrar01)
+texto[1].addEventListener ("mouseenter", entrar02)
+texto[2].addEventListener ("mouseenter", entrar03)
+
+const img = document.getElementsByClassName("luta")
+
+img[0].addEventListener ("mouseenter", entrar01)
+img[0].addEventListener ("mouseout", esconder01)
+
+img[1].addEventListener ("mouseenter", entrar02)
+img[1].addEventListener ("mouseout", esconder02)
+
+img[2].addEventListener ("mouseenter", entrar03)
+img[2].addEventListener ("mouseout", esconder03)
+
+function entrar01() {
+    texto[0].style.opacity = '1';
+    texto[0].style.visibility = 'visible';
+    img[0].style.width = "400px"
+}
+
+function entrar02(){
+    texto[1].style.opacity = '1';
+    texto[1].style.visibility = 'visible';
+    img[1].style.width = "400px"
+}
+
+function entrar03(){
+    texto[2].style.opacity = '1';
+    texto[2].style.visibility = 'visible';
+    img[2].style.width = "400px"
+}
+
+function esconder01(){
+    texto[0].style.opacity = '0';
+    texto[0].style.visibility = 'hidden';
+    img[0].style.width = "250px"
+}
+
+function esconder02(){
+    texto[1].style.opacity = '0';
+    texto[1].style.visibility = 'hidden';
+    img[1].style.width = "250px"
+}
+
+function esconder03(){
+    texto[2].style.opacity = '0';
+    texto[2].style.visibility = 'hidden';
+    img[2].style.width = "250px"
 }
